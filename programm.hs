946990de40fd0f7d76ba2673c6eede913a1b75ac
Maximilian Huber <hubi@by-hubi.de>
2013-05-09 18:46:10 +0200

    some changes

--- programm.hs
+++ programm.hs
@@ -1,17 +1,11 @@
 #!/usr/bin/env runhaskell
 module Main where
 import Data.Complex
-{-import Data.MemoTrie-}
 
-main :: IO ()
-main = do mapM_ print bothKoeff
-  where bothKoeff :: [(Int,Complex Double,Complex Double)]
-        bothKoeff = [(i, vKoeff i, uKoeff i)|i <- [-2..]]
-
+-- Parameter
 a = 1/8
 
-vKoeffs :: [Complex Double]
-vKoeffs = [vKoeff i|i <- [-1..]]
+-- returns n-th coefficient of v
 vKoeff :: Int -> Complex Double
 vKoeff n | n == -1   = 1/2
          | n ==  0   = 3/(uKoeff (-2)*4)
@@ -21,35 +15,13 @@ vKoeff n | n == -1   = 1/2
         xs = reverse [vKoeff (k-1)|k <- [1..n-1]]
         ys = [vKoeff (k-1)|k <- [1..n-1]]
 
-uKoeffs :: [Complex Double]
-uKoeffs = [uKoeff i|i <- [-2..]]
+-- returns n-th coefficient of u
 uKoeff :: Int -> Complex Double
-uKoeff n | n == -2   = 0:+(2*sqrt(2*a))
+uKoeff n | n == -2   = 0:+(sqrt(8*a))
          | n == -1   = -3/2
-         | otherwise = - (vKoeff n)
+         | otherwise = -(vKoeff n)
 
--- ############################################################################
-
-produkt :: [Complex Double] -> [Complex Double] -> [Complex Double]
-produkt = produkt' 0
-  where
-    produkt' :: Int -> [Complex Double] -> [Complex Double] -> [Complex Double]
-    produkt' i us vs = (sum (zipWith (*) (reverse (take (i+1) us)) vs)): produkt' (i+1) us vs
-
-check :: Int -> [Complex Double]
-check e = produkt [uKoeff i|i <- [-2..e]] [vKoeff i|i <- [-1..e]]
-
-{-
-pKoeffs :: [Complex Double]
-pKoeffs = [pKoeff i|i <- [-1..]]
-pKoeff :: Int -> Complex Double
-pKoeff n | n == -3   = 0:+(sqrt(2*a)*3)
-         | otherwise = 0
--}
-
--- ############################################################################
-
---evalU :: Double -> Int -> Complex Double
-evalU x e = zipWith (+) [uKoeff i|i <- [-2..e]] [x^i|i <- [-2..e]]
---evalV :: Double -> Int -> Complex Double
-evalV x e = zipWith (+) [vKoeff i|i <- [-1..e]] [x^i|i <- [-1..e]]
+main :: IO ()
+main = do mapM_ print bothKoeffs
+  where bothKoeffs :: [(Int,Complex Double,Complex Double)]
+        bothKoeffs = [(i, vKoeff i, uKoeff i)|i <- [-2..]]
