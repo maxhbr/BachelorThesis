53e9c9c23380eada491d2b534813f62a1c78b8ab
Maximilian Huber <hubi@by-hubi.de>
2013-05-28 23:08:25 +0200

    some changes
    Former-commit-id: b598e3d8afba6aa375101ce9b02cbf3bb208466a

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 import ComplRat
-import Data.MemoTrie (memo) -- https://github.com/conal/MemoTrie
+import Koeffs
 
 -- parallel
 import qualified Control.Monad.Parallel as P
@@ -9,24 +9,6 @@ import qualified Control.Monad.Parallel as P
 import System.Environment
 import System.IO
 import Data.Time
-
--- returns array with the coefficients of v(t)
--- first element in array is koefficient from t^{-1}
-vKoeffs :: ComplRat -> [ComplRat]
-vKoeffs uMin2 = 1/2:+:0 : [koeff i|i <- [0..]]
-  where koeff :: Int -> ComplRat
-        koeff = memo koeff'
-        koeff' :: Int -> ComplRat
-        koeff' n | n >   0   = (koeff (n-1)*(fromIntegral n+1)+summe)/uMin2
-                 | n ==  0   = -3/(uMin2*4)
-                 | n == -1   = 1/2
-                 | otherwise = 0
-                 where summe = sum [koeff (k-1)*(koeff (n-k-1))|k <- [1..n-1]]
-
--- returns array with the coefficients of u(t)
--- first element in array is koefficient from t^{-2}
-uKoeffs :: ComplRat -> [ComplRat]
-uKoeffs uMin2 = uMin2 : -3/2:+:0 : (tail $ vKoeffs uMin2)
 
 printData :: Int -> ComplRat -> IO()
 printData end uMin2 = mapM_ addLine $ take end $ zip3 [0..] (tail vals) vals
